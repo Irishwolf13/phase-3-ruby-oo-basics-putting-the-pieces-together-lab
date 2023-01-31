7af9c3c93b44ffe3d6399ff1f757e541e5368431
John Rooney <rooneyjohn@gmail.com>
2023-01-30 20:48:37 -0500

    commit tree 2c2afeeded93d2cf5ed63d0ebaa473907b6d8dce

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,17 @@
-
-
+class Book
+    attr_accessor :title
+    attr_accessor :author
+    attr_accessor :page_count
+    attr_accessor :genre
+    def initialize(title)
+        @title = title
+    end
+    def turn_page
+        puts "Flipping the page...wow, you read fast!"
+    end
+end
+mybook = Book.new("And Then There Were None")
+mybook.author = "Agatha Christie"
+mybook.page_count = 272
+mybook.genre = "Mystery"
+mybook.turn_page
